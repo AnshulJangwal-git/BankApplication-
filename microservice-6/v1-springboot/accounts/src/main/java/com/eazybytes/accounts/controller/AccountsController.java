fed8884ea41be039d7c832d5677901e35b3ba596
AnshulJangwal <anshuljangwal@gmail.com>
2025-03-31 18:20:19 +0530

    up

--- microservice-6/v1-springboot/accounts/src/main/java/com/eazybytes/accounts/controller/AccountsController.java
+++ microservice-6/v1-springboot/accounts/src/main/java/com/eazybytes/accounts/controller/AccountsController.java
@@ -31,11 +31,16 @@ import org.springframework.web.bind.annotation.*;
 )
 @RestController
 @RequestMapping(path="/api", produces = {MediaType.APPLICATION_JSON_VALUE})
-@AllArgsConstructor
+//@AllArgsConstructor
 @Validated
 public class AccountsController {
 
-    private IAccountsService iAccountsService;
+    private final IAccountsService iAccountsService;
+
+    public AccountsController(IAccountsService iAccountsService){
+        this.iAccountsService = iAccountsService;
+    }
+
     @Value("${build.version}")
     private String buildVersion;
 
