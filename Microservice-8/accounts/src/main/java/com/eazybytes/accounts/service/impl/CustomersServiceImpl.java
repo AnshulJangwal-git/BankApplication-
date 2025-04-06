d205589a4933c6a9badacc427e0c5d2df3580338
AnshulJangwal <anshuljangwal@gmail.com>
2025-04-06 18:40:56 +0530

    up

---
+++ Microservice-8/accounts/src/main/java/com/eazybytes/accounts/service/impl/CustomersServiceImpl.java
@@ -0,0 +1,14 @@
+package com.eazybytes.accounts.service.impl;
+
+import com.eazybytes.accounts.dto.CustomerDetailsDto;
+import com.eazybytes.accounts.service.ICustomersService;
+
+public class CustomersServiceImpl implements ICustomersService {
+
+    
+
+    @Override
+    public CustomerDetailsDto fetchCustomerDetails(String mobileNumber) {
+        return null;
+    }
+}
