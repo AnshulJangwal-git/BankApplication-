ceed4f41a21527bcb5532d6fd6e606c30c67c18d
AnshulJangwal <anshuljangwal@gmail.com>
2025-04-20 14:18:45 +0530

    up

--- Microservice-13/accounts/src/main/java/com/eazybytes/accounts/functions/AccountsFunctions.java
+++ Microservice-13/accounts/src/main/java/com/eazybytes/accounts/functions/AccountsFunctions.java
@@ -3,6 +3,7 @@ package com.eazybytes.accounts.functions;
 import com.eazybytes.accounts.service.IAccountsService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,10 +13,13 @@ import java.util.function.Consumer;
 public class AccountsFunctions {
     private static final Logger log = LoggerFactory.getLogger(AccountsFunctions.class);
 
+    @Autowired
+    private IAccountsService accountsService;
+
     @Bean
-    public Consumer<Long> updateCommunication(IAccountsService accountsService){
+    public Consumer<Long> updateCommunication(){
         return accountNumber -> {
-          log.info("Updating Communicatino status for the account number: "+ accountNumber.toString());
+          log.info("Updating Communication status for the account number: "+ accountNumber.toString());
           accountsService.updateCommunicationStatus(accountNumber);
         };
     }
